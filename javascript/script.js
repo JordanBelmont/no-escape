b993afc6163c61107a543e156c73d257650daff9
Jordan Belmont <55675483+JordanBelmont@users.noreply.github.com>
2022-12-09 19:36:24 +0000

    Changed 'title' to 'name' for javascript

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@ let previous = document.getElementById('previous');
 let next = document.getElementById('next');
 
 let image = document.getElementById('image');
-let title = document.getElementById('title');
+let name = document.getElementById('name');
 let story = document.getElementById('story');
 let difficultyOne = document.getElementById('difficultyOne');
 let difficultyTwo = document.getElementById('difficultyTwo');
@@ -26,32 +26,32 @@ let room_index = 0;
 let rooms = [
    {
       img: "images/Toy-Box.png",
-      title: "Toy Box",
+      name: "Toy Box",
       storyone: "For weeks, reports of the serial killer known as 'The Toyman' have flooded news outlets, the last thing you ever expected was to wind up as one of his victims.",
       storytwo: "Trapped in a hand-crafted maze of building blocks, ball pits and ragged toys, you must escape from this nightmare, before playtime is over forever.",
       difficultyone: "icons/skull.png",
       difficultytwo: "icons/skull.png",
       difficultythree: "icons/skull.png",
-      difficultyfour: "",
-      difficultyfive: "",
+      difficultyfour: "icons/skull.png",
+      difficultyfive: "icons/skull.png",
       fearone: "icons/skull.png",
       feartwo: "icons/skull.png",
       fearthree: "icons/skull.png",
       fearfour: "icons/skull.png",
-      fearfive: "",
+      fearfive: "icons/skull.png",
       time: "60 mins",
       players: "1-6"
    },
    {
       img: "images/Doom-Town.png",
-      title: "Doom Town",
+      name: "Doom Town",
       storyone: "As part of the atomic testing site research crew, it was your task to simply document finding, but you just couldn't shake the feeling you were being watched.",
       storytwo: "Trapped in an atomic-blistered ruin, you'll need to conquer your fears in order to escape your ordeal before you become a permanent resident of Doom Town.",
       difficultyone: "icons/skull.png",
       difficultytwo: "icons/skull.png",
       difficultythree: "icons/skull.png",
       difficultyfour: "icons/skull.png",
-      difficultyfive: "",
+      difficultyfive: "icons/skull.png",
       fearone: "icons/skull.png",
       feartwo: "icons/skull.png",
       fearthree: "icons/skull.png",
@@ -62,7 +62,7 @@ let rooms = [
    },
    {
       img: "images/Moloch-Trials.png",
-      title: "The Moloch Trials",
+      name: "The Moloch Trials",
       storyone: "Rumours have been spreading about the increase in missing children coinciding with the emergence of a secret society known only as 'The Embers of Tophet'.",
       storytwo: "As a local journalist, you've been invited to shed light on the order to quell some of the allegations. But you're about to learn why this order has such an exclusive membership.",
       difficultyone: "icons/skull.png",
@@ -73,8 +73,8 @@ let rooms = [
       fearone: "icons/skull.png",
       feartwo: "icons/skull.png",
       fearthree: "icons/skull.png",
-      fearfour: "",
-      fearfive: "",
+      fearfour: "icons/skull.png",
+      fearfive: "icons/skull.png",
       time: "60 mins",
       players: "1-6"
    }
@@ -86,7 +86,7 @@ changeRoom = (room_index) => {
 
    // Change room
    image.src = rooms[room_index].img;
-   title.textContent = rooms[room_index].title;
+   name.textContent = rooms[room_index].name;
    storyone.textContent = rooms[room_index].storyone;
    storytwo.textContent = rooms[room_index].storytwo;
    difficultyOne.src = rooms[room_index].difficultyone;
@@ -103,7 +103,7 @@ changeRoom = (room_index) => {
    players.textContent = rooms[room_index].players;
 }
 
-// ! NEED TO FIX SLIDERS
+// ! NEED TO FIX SLIDERS !
 
 // Slider Dots
 document.querySelectorAll('.dot').forEach(function (selector, room_index) {
