bc142ed5fb149eb597932b06215c6c21043b126a
Jordan Belmont <55675483+JordanBelmont@users.noreply.github.com>
2022-12-10 11:14:16 +0000

    Finalised burger functionality

--- javascript/script.js
+++ javascript/script.js
@@ -145,6 +145,6 @@ let burger = document.getElementById('burgerButton');
 
 burger.addEventListener('click', () => {
    burger.classList.toggle('opened');
-   burger.setAttribute('aria-expanded', nav.classList.contains('nav-ul'))
-   nav.classList.toggle('nav-ul');
+   burger.setAttribute('aria-expanded', nav.classList.contains('abattoir-nav'))
+   nav.classList.toggle('abattoir-nav');
 });
